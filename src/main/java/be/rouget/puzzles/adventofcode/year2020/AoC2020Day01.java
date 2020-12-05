3b8f4175142b85a4f324c7097b31021ad170ee90
TumbleMagnet <trouget@gmail.com>
2020-12-05 16:41:46 +0100

    First solution of 2020 day 1 - cleaned up

--- src/main/java/be/rouget/puzzles/adventofcode/year2020/AoC2020Day01.java
+++ src/main/java/be/rouget/puzzles/adventofcode/year2020/AoC2020Day01.java
@@ -1,10 +1,15 @@
 package be.rouget.puzzles.adventofcode.year2020;
 
 import be.rouget.puzzles.adventofcode.util.ResourceUtils;
+import com.google.common.base.Stopwatch;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class AoC2020Day01 {
 
@@ -20,65 +25,59 @@ public class AoC2020Day01 {
         List<String> input = ResourceUtils.readLines("aoc_2020_day01_input.txt");
         LOG.info("Input has {} lines", input.size());
         AoC2020Day01 aoc = new AoC2020Day01(input);
+
         LOG.info("Result for part 1 is: " + aoc.computeResultForPart1());
         LOG.info("Result for part 2 is: " + aoc.computeResultForPart2());
     }
 
     public long computeResultForPart1() {
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        Pair selectedValues = findComplementsForTotal(toSortedValues(input), 2020L);
+        if (selectedValues != null) {
+            LOG.info("Found values after {} milliseconds", stopwatch.elapsed(TimeUnit.MILLISECONDS));
+            return selectedValues.getProduct();
+        }
+        throw new IllegalStateException("Found no matching values");
+    }
 
-        // Convert input into an array of longs
-        Long[] values = input.stream().map(Long::parseLong).toArray(Long[]::new);
-
-        // Find pair whose sum equals 2020
-        long count = 0;
-        for (int i = 0; i < values.length-1; i++) {
-            for (int j = i+1; j < values.length; j++) {
-                Pair candidate = new Pair(values[i], values[j]);
-                count++;
-//                LOG.info("Trying pair {}...", candidate);
-                if (candidate.getSum().equals(2020L)) {
-                    LOG.info("Found pair after {} tries: {}", count, candidate);
-                    return candidate.getProduct();
-                }
+    private Pair findComplementsForTotal(List<Long> inputValues, long total) {
+        Map<Long, Long> valueMap = inputValues.stream().collect(Collectors.toMap(Function.identity(), Function.identity()));
+        for (Long candidate: inputValues) {
+            Long complement = total - candidate;
+            if (valueMap.containsKey(complement)) {
+                return new Pair(candidate, complement);
             }
         }
-        throw new IllegalStateException("Found no matching pair");
+        return null;
     }
+
     public long computeResultForPart2() {
 
-        // Convert input into an array of longs
-        Long[] values = input.stream().map(Long::parseLong).toArray(Long[]::new);
+        Stopwatch stopwatch = Stopwatch.createStarted();
 
-        // Find pair whose sum equals 2020
-        long count = 0;
-        for (int i = 0; i < values.length-1; i++) {
-            for (int j = i+1; j < values.length; j++) {
-                for (int z = j+1; z < values.length; z++) {
-                    Triplet candidate = new Triplet(values[i], values[j], values[z]);
-                    count++;
-//                LOG.info("Trying triplet {}...", candidate);
-                    if (candidate.getSum().equals(2020L)) {
-                        LOG.info("Found triplet after {} tries: {}", count, candidate);
-                        return candidate.getProduct();
-                    }
-                }
+        List<Long> allValues = toSortedValues(input);
+        for (Long candidate1 : allValues) {
+
+            // Find pair in remaning values whose sum is 2020 - candidate1
+            List<Long> remaining = otherValues(allValues, candidate1);
+            long subTotal = 2020L - candidate1;
+
+            Pair pair = findComplementsForTotal(remaining, subTotal);
+            if (pair != null) {
+                LOG.info("Found values after {} milliseconds", stopwatch.elapsed(TimeUnit.MILLISECONDS));
+                return pair.getProduct() * candidate1;
             }
         }
-        throw new IllegalStateException("Found no matching pair");
+        throw new IllegalStateException("Found no matching values");
     }
 
     public static class Pair {
 
         private Long first;
         private Long second;
-
         public Pair(Long first, Long second) {
             this.first = first;
             this.second = second;
-        }
-
-        public Long getSum() {
-            return first + second;
         }
 
         public Long getProduct() {
@@ -94,32 +93,11 @@ public class AoC2020Day01 {
         }
     }
 
-    public static class Triplet {
-        private Long first;
-        private Long second;
-        private Long third;
+    private static List<Long> toSortedValues(List<String> input) {
+        return input.stream().map(Long::parseLong).collect(Collectors.toList());
+    }
 
-        public Triplet(Long first, Long second, Long third) {
-            this.first = first;
-            this.second = second;
-            this.third = third;
-        }
-
-        public Long getSum() {
-            return first + second + third;
-        }
-
-        public Long getProduct() {
-            return first * second * third;
-        }
-
-        @Override
-        public String toString() {
-            return "Triplet{" +
-                    "first=" + first +
-                    ", second=" + second +
-                    ", third=" + third +
-                    '}';
-        }
+    private static List<Long> otherValues(List<Long> input, Long value) {
+        return input.stream().filter(v -> !v.equals(value)).collect(Collectors.toList());
     }
 }
