133cd9623472c81f7149eda179013901b4c4f8ef
TumbleMagnet <trouget@gmail.com>
2023-01-16 20:40:50 +0100

    AOC 2022 day 15 - optimize part 2

--- src/main/java/be/rouget/puzzles/adventofcode/year2022/day15/BeaconExclusionZone.java
+++ src/main/java/be/rouget/puzzles/adventofcode/year2022/day15/BeaconExclusionZone.java
@@ -26,6 +26,7 @@ public class BeaconExclusionZone {
         BeaconExclusionZone aoc = new BeaconExclusionZone(input);
         LOG.info("Result for part 1 is: {}", aoc.computeResultForPart1());
         LOG.info("Result for part 2 is: {}", aoc.computeResultForPart2());
+        LOG.info("Result for part 2 (optimized) is: {}", aoc.computeResultForPart2Optimized());
     }
 
     public BeaconExclusionZone(List<String> input) {
@@ -94,6 +95,7 @@ public class BeaconExclusionZone {
     public long computeResultForPart2() {
 
         // Find beacon position by scanning all rows and finding the first row with exactly one possible position 
+        // For each row, merge the sensors exclusion zones and remove them from the search area
         Position beaconPosition = null;
         for (int y = 0; y < PART2_MAX_COORDINATE + 1; y++) {
             List<Range> possibleRanges = List.of(new Range(0, PART2_MAX_COORDINATE));
@@ -119,6 +121,11 @@ public class BeaconExclusionZone {
         }
         
         // Return tuning frequency of beacon position    
+        return tuningFrequency(beaconPosition);
+    }
+
+    @SuppressWarnings("java:S1905")
+    private static long tuningFrequency(Position beaconPosition) {
         return (long) beaconPosition.getX() * (long) PART2_MAX_COORDINATE + (long) beaconPosition.getY();
     }
 
@@ -128,4 +135,85 @@ public class BeaconExclusionZone {
                 .flatMap(List::stream)
                 .toList();
     }
+
+    public long computeResultForPart2Optimized() {
+        
+        // We can take advantage from the fact there exists only one possible position in the search space.
+        // It means that the position we search is necessarily on the outer ring of a zone covered a sensor.
+        Position position = sensorReports.stream()
+                .map(this::scanOuterRing)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .findFirst().orElseThrow();
+
+        // Return tuning frequency of beacon position    
+        return tuningFrequency(position);
+    }
+
+    private Optional<Position> scanOuterRing(SensorReport sensorReport) {
+
+        Position sensorPosition = sensorReport.sensor();
+        int sensorRange = sensorPosition.computeManhattanDistance(sensorReport.closestBeacon());
+        
+        // Upper half
+        for (int y = sensorPosition.getY() - sensorRange; y <= sensorPosition.getY(); y++) {
+            Range range = sensorReport.computeBeaconExclusionZoneForRow(y).orElseThrow();
+            Position leftPosition = new Position(range.from(), y - 1);
+            if (isSolution(leftPosition)) {
+                return Optional.of(leftPosition);
+            }
+            Position rightPosition = new Position(range.to(), y - 1);
+            if (isSolution(rightPosition)) {
+                return Optional.of(rightPosition);
+            }
+        }
+        
+        // Lower half
+        for (int y = sensorPosition.getY(); y <= sensorPosition.getY() + sensorRange; y++) {
+            Range range = sensorReport.computeBeaconExclusionZoneForRow(y).orElseThrow();
+            Position leftPosition = new Position(range.from(), y + 1);
+            if (isSolution(leftPosition)) {
+                return Optional.of(leftPosition);
+            }
+            Position rightPosition = new Position(range.to(), y + 1);
+            if (isSolution(rightPosition)) {
+                return Optional.of(rightPosition);
+            }
+        }
+
+        // Sensor row
+        Position leftPosition = new Position(sensorPosition.getX() - sensorRange - 1, sensorPosition.getY());
+        if (isSolution(leftPosition)) {
+            return Optional.of(leftPosition);
+        }
+        Position rightPosition = new Position(sensorPosition.getX() + sensorRange + 1, sensorPosition.getY());
+        if (isSolution(rightPosition)) {
+            return Optional.of(rightPosition);
+        }
+
+        // No match on outer ring of this sensor
+        return Optional.empty();
+    }
+
+    private boolean isSolution(Position candidate) {
+
+        if (candidate.getX() < 0 || candidate.getX() > PART2_MAX_COORDINATE
+                || candidate.getY() < 0 || candidate.getY() > PART2_MAX_COORDINATE) {
+            // Not in search area
+            return false;
+        }
+        
+        for (SensorReport sensorReport : sensorReports) {
+            int sensorRange = sensorReport.sensor().computeManhattanDistance(sensorReport.closestBeacon());
+            int distanceToCandidate = sensorReport.sensor().computeManhattanDistance(candidate);
+            if (distanceToCandidate <= sensorRange) {
+                // Too close to a sensor
+                return false;
+            }
+        }
+        
+        // Far enough of all sensors
+        return true;
+    }
+
 }
